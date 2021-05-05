c5dce9337f74905163b314db7da2ed8326f596d7
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-05-05 16:25:40 -0400

    routes working properly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Header from "./components/Header.js";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+import Projects from "./pages/Projects.js";
+import NoMatch from "./pages/NoMatch.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Footer from "./components/Footer.js";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-    </div>
+      <Switch>
+        <Route exact path={["/", "/home"]} component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route path="*" component={NoMatch} />
+      </Switch>
+    </Router>
   );
 }
 
